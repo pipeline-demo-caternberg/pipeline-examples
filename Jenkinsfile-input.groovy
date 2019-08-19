8bd3abfd1941a42222ff06d49189b727d2ab3fa1
cccaternberg <acaternberg@cloudbees.com>
2019-08-19 14:50:45 +0200

    add

--- Jenkinsfile-input.groovy
+++ Jenkinsfile-input.groovy
@@ -13,30 +13,16 @@ pipeline {
                             name: 'Give me a value')]
                 }
                 echo "${returnValue}"
-
             }
         }
 
         stage('Say Hello2') {
-            script {
-                //If agent other than "any" the ìput`should take place outside a agent definition!!!
-                returnValue = input message:
-                'Need some input' ,
-                parameters:
-                [string(defaultValue: '',
-                        description: '',
-                        name: 'Give me a value')]
-            }
             agent { label "cloudbees-core" }
-
             steps {
-
-                container("curl") {
+            container("curl") {
                     echo "Input is not blocking the agent: Returnvalue is:${returnValue}"
-                }
-
-
             }
+           }
         }
     }
 }
