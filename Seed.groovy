1e334ccee114c0c08c0208daec9e35b89613a215
cccaternberg <acaternberg@cloudbees.com>
2019-08-20 14:49:24 +0200

    add

--- Seed.groovy
+++ Seed.groovy
@@ -1,17 +1,4 @@
-pipeline {
-    agent any
+/**
 
-    environment {
-        A_VALUE = 'Some Value'
-    }
-
-    stages {
-        stage('Build') {
-            steps {
-                echo "${A_VALUE}"
-                echo "${env.BUILD_ID}"
-                echo "${currentBuild.result}"
-            }
-        }
-    }
-}
+ TODO
+ */
