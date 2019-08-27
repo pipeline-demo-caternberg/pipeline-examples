8965026b3e5a72b631528e664d16854faf133f2b
cccaternberg <acaternberg@cloudbees.com>
2019-08-27 13:31:05 +0200

    withEnv

--- Seed.groovy
+++ Seed.groovy
@@ -43,7 +43,7 @@ folder("${genFolder}") {
 }
 
 GHRepository ghRepository = ghOrganization.getRepository("pipeline-examples")
-List<GHContent> ghContentList = ghRepository.getDirectoryContent("jobs")
+List<GHContent> ghContentList = ghRepository.getDirectoryContent("/")
 //List<GHContent> ghContentList = ghRepository.getFileContent '/jobs/'
 for (ghContent in ghContentList) {
     println(ghContent.name)
