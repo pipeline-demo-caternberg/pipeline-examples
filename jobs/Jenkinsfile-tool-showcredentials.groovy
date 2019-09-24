ada6cc0fb6de7c485bf46ff03d3a99d2e009e58e
cccaternberg <acaternberg@cloudbees.com>
2019-09-24 11:10:57 +0200

    change impl

--- jobs/Jenkinsfile-tool-showcredentials.groovy
+++ jobs/Jenkinsfile-tool-showcredentials.groovy
@@ -1,27 +1,12 @@
-import com.cloudbees.plugins.credentials.CredentialsProvider
-import com.cloudbees.plugins.credentials.Credentials
-import com.cloudbees.plugins.credentials.domains.Domain
-import jenkins.model.Jenkins
-def indent = { String text, int indentationCount ->
-    def replacement = "\t" * indentationCount
-    text.replaceAll("(?m)^", replacement)
+def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+        Jenkins.instance,
+        null,
+        null
+);
+for (c in creds) {
+    println( ( c.properties.privateKeySource ? "ID: " + c.id + ", UserName: " + c.username + ", Private Key: " + c.getPrivateKey() : ""))
 }
-
-Jenkins.get().allItems().collectMany{ CredentialsProvider.lookupStores(it).toList()}.unique().forEach { store ->
-    Map<Domain, List<Credentials>> domainCreds = [:]
-    store.domains.each { domainCreds.put(it, store.getCredentials(it))}
-    if (domainCreds.collectMany{ it.value}.empty) {
-        return
-    }
-    def shortenedClassName = store.getClass().name.substring(store.getClass().name.lastIndexOf(".") + 1)
-    println "Credentials for store context: ${store.contextDisplayName}, of type $shortenedClassName"
-    domainCreds.forEach { domain , creds ->
-        println indent("Domain: ${domain.name}", 1)
-        creds.each { cred ->
-            cred.properties.each { prop, val ->
-                println indent("$prop = \"$val\"", 2)
-            }
-            println indent("-----------------------", 2)
-        }
-    }
+for (c in creds) {
+    println( ( c.properties.password ? "ID: " + c.id + ", UserName: " + c.username + ", Password: " + c.password : ""))
 }
