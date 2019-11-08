cccf1fcfe145f60f5addca73b6e08b31decf86e4
cccaternberg <acaternberg@cloudbees.com>
2019-11-08 20:36:52 +0100

    rafactoring

--- jobs/Jenkinsfile-devoptics-consumer-gate1.groovy
+++ jobs/Jenkinsfile-devoptics-consumer-gate1.groovy
@@ -6,7 +6,7 @@ pipeline {
     */
     agent {
         kubernetes {
-            yamlFile 'yaml/podTemplate.yml'
+            yamlFile 'resources/yaml/podTemplate.yml'
         }
     }
     triggers {
