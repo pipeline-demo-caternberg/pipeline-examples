1eab3eb1dd9fd918b2206faeac736554093ca1fc
cccaternberg <acaternberg@cloudbees.com>
2019-11-15 14:41:47 +0100

    add

---
+++ jobs/Jenkinsfile-docker-build-customagent.groovy
@@ -0,0 +1,40 @@
+def label = "kaniko-${UUID.randomUUID().toString()}"
+podTemplate(name: 'kaniko', label: label, yaml: """
+kind: Pod
+metadata:
+  name: kaniko
+spec:
+  containers:
+  - name: kaniko
+    image: gcr.io/kaniko-project/executor:debug
+    imagePullPolicy: Always
+    command:
+    - /busybox/cat
+    tty: true
+    volumeMounts:
+      - name: jenkins-docker-cfg
+        mountPath: /kaniko/.docker
+  volumes:
+  - name: jenkins-docker-cfg
+    projected:
+      sources:
+      - secret:
+          name: docker-credentials
+          items:
+            - key: .dockerconfigjson
+              path: config.json
+""") { 
+  node(label) {
+    stage('Build with Kaniko') {
+        container(name: 'kaniko', shell: '/busybox/sh') {
+            sh 'ls -lR'
+           git 'https://github.com/pipeline-demo-caternberg/pipeline-examples.git'
+           withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+            sh '''#!/busybox/sh
+                /kaniko/executor  --dockerfile $(pwd)/resources/dockerfiles/Dockerfile-custom-jnlp-agent --insecure --skip-tls-verify --cache=false  --context $(pwd) --destination caternberg/jenkins-agent-customized:BUILD_NUMBER-${BUILD_NUMBER}
+            '''
+           }
+        }
+      }
+    }
+  }
