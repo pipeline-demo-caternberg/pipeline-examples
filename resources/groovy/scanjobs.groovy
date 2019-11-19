d9b04cbc3c8c0f86d1bdcf39d96ed9212d8afd01
cccaternberg <acaternberg@cloudbees.com>
2019-11-19 16:47:10 +0100

    add

--- resources/groovy/scanjobs.groovy
+++ resources/groovy/scanjobs.groovy
@@ -5,7 +5,7 @@ def scan() {
             println it.lastBuild.timestamp.get(Calendar.YEAR)+"-"+(it.lastBuild.timestamp.get(Calendar.MONTH)+1)+"-"+it.lastBuild.timestamp.get(Calendar.DATE) + " " + it.fullName + " " + it.class.name;
             //I know, this is very ugly to catch NPE, sry
         }catch (Exception e){
-            println "Error catched: " +  e.message
+            println "Error catches: " +  e.message
         }
     }
 }
