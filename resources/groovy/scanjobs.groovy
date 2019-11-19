cd07274eb52b15aace862daed215d7908e6a7582
cccaternberg <acaternberg@cloudbees.com>
2019-11-19 16:46:22 +0100

    cleanup

--- resources/groovy/scanjobs.groovy
+++ resources/groovy/scanjobs.groovy
@@ -1,20 +1,13 @@
 
 def scan() {
     Jenkins.instance.getAllItems(AbstractItem.class).sort().each {it ->
-
         try {
             println it.lastBuild.timestamp.get(Calendar.YEAR)+"-"+(it.lastBuild.timestamp.get(Calendar.MONTH)+1)+"-"+it.lastBuild.timestamp.get(Calendar.DATE) + " " + it.fullName + " " + it.class.name;
-
-            //Iknow, this is very ugly to catch NPE . , sry
+            //I know, this is very ugly to catch NPE, sry
         }catch (Exception e){
             println "Error catched: " +  e.message
         }
-
     }
-
-
-
-
 }
 
 return this
