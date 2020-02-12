3cb30668718cff6bd28e8a1a50b63189da380224
cccaternberg <acaternberg@cloudbees.com>
2020-02-12 13:02:30 +0100

    rename

---
+++ jobs/Jenkinsfile-curl-jq-variable.groovy
@@ -0,0 +1,25 @@
+
+
+
+
+pipeline {
+    agent { node { label 'master' } }
+    stages {
+        stage('hello World') {
+            steps {
+                script {
+                    DEPLOYMENT_ALLOWED = sh(script: """
+echo '{"result":{"deploymentAllowed":"false","caseType":"not found"}}' | jq .result.deploymentAllowed
+"false"
+          """, returnStdout: true)
+                }
+                environment  {
+                    DEPLOYMENT_ALLOWED = ${DEPLOYMENT_ALLOWED}
+                }
+                echo sh(script: 'env|sort', returnStdout: true)
+                echo "DEPLOYMENT_ALLOWED: ${DEPLOYMENT_ALLOWED}"
+            }
+        }
+    }
+}
+
