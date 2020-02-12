190d9f6494e0724c7df8e9a5b0fa765567ffaceb
cccaternberg <acaternberg@cloudbees.com>
2020-02-12 21:25:52 +0100

    fixD

--- jobs/Jenkinsfile-when-expression2.groovy
+++ jobs/Jenkinsfile-when-expression2.groovy
@@ -13,13 +13,8 @@ pipeline {
         }
 
         stage ("next") {
-          /*  environment  {
-                DEPLOYMENT_ALLOWED = "${DEPLOYMENT_ALLOWED}"
-            }
-            */
             when { expression { return Boolean.valueOf("${DEPLOYMENT_ALLOWED}") } }
             steps {
-             //   echo sh(script: 'env|sort', returnStdout: true)
                 sh  "echo Hello, bitwiseman! ${DEPLOYMENT_ALLOWED}"
             }
         }
