221d9a18cbcad975b4105da7e307713580d257e6
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2020-06-17 08:50:47 +0200

    Create Jenkinsfile-docker-dind-agent.groovy

---
+++ jobs/Jenkinsfile-docker-dind-agent.groovy
@@ -0,0 +1,21 @@
+// Build a Maven project using the standard image and Scripted syntax.
+// Rather than inline YAML, you could use: yaml: readTrusted('jenkins-pod.yaml')
+// Or, to avoid YAML: containers: [containerTemplate(name: 'maven', image: 'maven:3.6.3-jdk-8', command: 'sleep', args: 'infinity')]
+podTemplate(yaml: '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: dind
+    image: benhall/dind-jenkins-agent
+    command:
+    - sleep
+    args:
+    - infinity
+''') {
+    node(POD_LABEL) {
+        container('dind') {
+            sh 'docker build -t caternberg/dindtest .'
+        }      
+    }
+}
