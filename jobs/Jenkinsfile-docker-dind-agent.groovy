120ee712214b65ea916f89ec83fdf715fc1199c5
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2020-06-17 09:17:48 +0200

    Update Jenkinsfile-docker-dind-agent.groovy

--- jobs/Jenkinsfile-docker-dind-agent.groovy
+++ jobs/Jenkinsfile-docker-dind-agent.groovy
@@ -15,8 +15,8 @@ spec:
 ''') {
     node(POD_LABEL) {
         container('dind') {
-          git https://github.com/pipeline-demo-caternberg/pipeline-examples.git
-          sh "ls -l ../"
+          git 'https://github.com/pipeline-demo-caternberg/pipeline-examples.git'
+          //sh "ls -l ../"
             sh 'docker build -t caternberg/dindtest -f $(pwd)/resources/dockerfiles/Dockerfile-custom-jnlp-agent  .'
         }      
     }
