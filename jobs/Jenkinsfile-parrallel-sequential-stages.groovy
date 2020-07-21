9f58b9b7cf6812cf1c2847ff4e4554a594b72907
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2020-07-21 17:39:38 +0200

    Create Jenkinsfile-parrallel-sequential-stages.groovy

---
+++ jobs/Jenkinsfile-parrallel-sequential-stages.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent none
+
+    stages {
+        stage("build and deploy on Windows and Linux") {
+            parallel {
+                stage("threadA") {
+                     agent {
+                          kubernetes {
+                            yamlFile 'resources/yaml/podTemplate-maven.yml'
+                         }
+                    }
+                    stages {
+                        stage("build") {
+                            steps {
+                                sh "echo build threadA"
+                            }
+                        }
+                        stage("deploy") {
+                            when {
+                                branch "master"
+                            }
+                            steps {
+                                sh "echo deploy threadA"
+                            }
+                        }
+                    }
+                }
+
+                stage("threadB") {
+                     agent {
+                          kubernetes {
+                            yamlFile 'resources/yaml/podTemplate-maven.yml'
+                         }
+                    }
+                    stages {
+                        stage("build") {
+                            steps {
+                                sh "echo build threadB"
+                            }
+                        }
+                        stage("deploy") {
+                             when {
+                                 branch "master"
+                             }
+                             steps {
+                                  sh "echo deploy threadB"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
