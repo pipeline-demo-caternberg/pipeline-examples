2abe36c9ae08d7879c8e81fcba12fa5e02243f5a
cccaternberg <acaternberg@cloudbees.com>
2020-12-03 15:47:16 +0100

    fix

--- jobs/Jenkinsfile-tool-curlEventCause.groovy
+++ jobs/Jenkinsfile-tool-curlEventCause.groovy
@@ -1,33 +1,32 @@
 
-
-
+library '_github_com_pipeline-templates-apps_pipeline-library' _
+def ostools = libraryResource 'podtemplates/podTemplate-os-tools.yaml'
+//TDO: move token to credentials store!
 def adminToken = "admin:11d2b842e59277c949c1cd2e2aa1497c6a"
 pipeline {
-    agent any
-
+    agent {
+        kubernetes {
+            yaml ostools
+        }
+    }
     stages {
         stage('Hello') {
             steps {
                 echo 'Hello World'
                 // withCredentials([string(credentialsId: 'jenkinsAdminAndToken', variable: 'ADMINTOKEN')]) {
                 echo "ADMINTOKEN: ${adminToken}"
-                sh "curl  -o ./jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x ./jq"
+                //    sh "curl  -o ./jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x ./jq"
+                sh "ls -l /usr/bin"
                 script {
-                    DEPLOYMENT_ALLOWED = sh(script: """
-                          echo true
+                    EVENT_CAUSE_JSON = sh(script: """
+                            curl -vl  -u  ${adminToken}  --silent  ${BUILD_URL}/api/json
+                            # curl -v  -u  ${adminToken}  --silent  ${BUILD_URL}/api/json | ./jq '.actions[0].causes[0].event'
             """, returnStdout: true).trim()
                 }
-                sh "echo ${DEPLOYMENT_ALLOWED}"
-
-                script {
-                      EVENT_CAUSE_JSON = sh(script: """
-                            curl -v  -u  ${adminToken}  --silent  ${BUILD_URL}/api/json | ./jq '.actions[0].causes[0].event'
-            """, returnStdout: true).trim()
-                }
-             //   environment {
-           //         EVENT_CAUSE_JSON = ${EVENT_CAUSE_JSON}
-             //   }
-                echo sh(script: 'env|sort', returnStdout: true)
+                //  environment {
+                //     EVENT_CAUSE_JSON = ${EVENT_CAUSE_JSON}
+                // }
+                // echo sh(script: 'env|sort', returnStdout: true)
                 echo "EVENT_CAUSE_JSON: ${EVENT_CAUSE_JSON}"
 
 
