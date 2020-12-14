5793b0a72edcce34bbabc208507cfca3ee5145d5
cccaternberg <acaternberg@cloudbees.com>
2020-12-14 15:04:13 +0100

    add param

--- jobs/Jenkinsfile-docker-custom-agent-execution.groovy
+++ jobs/Jenkinsfile-docker-custom-agent-execution.groovy
@@ -4,12 +4,17 @@ pipeline {
             yamlFile 'resources/yaml/podTemplate-customagent.yml'
         }
     }
+    parameters {
+        string(name: 'kubectl_command', defaultValue: 'cluster-info',
+                description: 'put a kubectl command')
+    }
     stages {
         stage('Say Hello') {
             steps {
                 container('custom-agent') {
                     echo 'Hello World!'
                     sh "kubectl version"
+                    sh "kubectl ${params.kubectl_command}"
                 }
             }
         }
