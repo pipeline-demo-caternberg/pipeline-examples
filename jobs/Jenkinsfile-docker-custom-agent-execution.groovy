17f9b65bfbece4897e7fed9cfc20bf7736c73479
cccaternberg <acaternberg@cloudbees.com>
2020-12-15 11:28:22 +0100

    add

--- jobs/Jenkinsfile-docker-custom-agent-execution.groovy
+++ jobs/Jenkinsfile-docker-custom-agent-execution.groovy
@@ -8,8 +8,13 @@ pipeline {
         }
     }
     parameters {
-        string(name: 'kubectl_command', defaultValue: 'cluster-info',
-                description: 'put a kubectl command')
+        string(name: 'kubectl_command', defaultValue: 'cluster-info',   description: 'put a kubectl command'),
+        //F.e AWS key can be taken here as well from Credstore
+        string(name: 'user', defaultValue: 'caternberg',   description: 'user')
+        password(name: 'passwd', defaultValue: 'secret', description: 'Enter Password')
+
+
+
     }
     stages {
 
@@ -20,7 +25,8 @@ pipeline {
                     withKubeConfig(credentialsId: "${credentialID}" ,namespace: 'cloudbees-core', serverUrl: 'https://35.196.164.234/') {
                         sh "kubectl version"
                         sh "kubectl ${params.kubectl_command}"
-                        sh "kubectl ${createDockerCredenetials}"
+                        sh "kubectl delete secret docker-credentials"
+                        sh "kubectl create secret docker-registry docker-credentials --docker-username=${params.user}  --docker-password=${params.passwd}"
                     }
                 }
             }
