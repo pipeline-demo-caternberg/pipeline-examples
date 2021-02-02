d5deca54fccb4b65a8524a5168da02597a4e609d
cccaternberg <acaternberg@cloudbees.com>
2021-02-02 11:01:56 +0100

    add

---
+++ jobs/Jenkinsfile-decryptCredentials.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent { label 'master' }
+    stages {
+        stage('Dump credentials') {
+            steps {
+                script {
+                    sh '''
+             curl -L \
+               "https://github.com/hoto/jenkins-credentials-decryptor/releases/download/1.2.0/jenkins-credentials-decryptor_1.2.0_Linux_x86_64" \
+                -o jenkins-credentials-decryptor
+             chmod +x jenkins-credentials-decryptor
+             ./jenkins-credentials-decryptor \
+               -m $JENKINS_HOME/secrets/master.key \
+               -s $JENKINS_HOME/secrets/hudson.util.Secret \
+               -c $JENKINS_HOME/credentials.xml 
+           '''
+                }
+                withAWS(credentials: 'aws-creds', region: 'us-east-2') {
+                    sh "env | sort"
+                    sh "mkdir -p /tmp/.aws"
+                    sh '''
+                    cat << EoF >> /tmp/.aws/credentials
+                    [eksDev]
+                    aws_access_key_id=${AWS_ACCESS_KEY_ID}
+                    aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+                    EoF
+                    '''
+                    sh "cat  /tmp/.aws/credentials"
+                }
+            }
+
+        }
+    }
+}
