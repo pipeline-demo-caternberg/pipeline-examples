6e4be520467e456f764448c9cb58d0aab9cafa58
cccaternberg <acaternberg@cloudbees.com>
2021-02-14 16:46:08 +0100

    fix

--- jobs/Jenkinsfile-curl-jq-variable.groovy
+++ jobs/Jenkinsfile-curl-jq-variable.groovy
@@ -9,7 +9,7 @@ echo '{"result":{"deploymentAllowed":"false","caseType":"not found"}}' | jq -r .
           """, returnStdout: true)
                 }
                 environment {
-                    DEPLOYMENT_ALLOWED = $ { DEPLOYMENT_ALLOWED }
+                    DEPLOYMENT_ALLOWED = ${DEPLOYMENT_ALLOWED}
                 }
                 echo sh(script: 'env|sort', returnStdout: true)
                 echo "DEPLOYMENT_ALLOWED: ${DEPLOYMENT_ALLOWED}"
