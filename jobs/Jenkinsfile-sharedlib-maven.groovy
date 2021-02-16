11f49323fad0b32cfff5b3006c5aa69dbddc8df5
cccaternberg <acaternberg@cloudbees.com>
2021-02-16 10:11:42 +0100

    add

---
+++ jobs/Jenkinsfile-sharedlib-maven.groovy
@@ -0,0 +1,2 @@
+library '_github.com_pipeline-demo-caternberg_pipeline_examples' _
+mavenProject (gitRepo:"git@github.com:pipeline-demo-caternberg/maven-project.git",credentialId:"githubuserssh",branch:"master")
--- jobs/Jenkinsfile-sharedlib-simple.groovy
+++ jobs/Jenkinsfile-sharedlib-simple.groovy
@@ -1,2 +1,3 @@
 library '_github.com_pipeline-demo-caternberg_pipeline_examples' _
 mySimplePipeline(param1: 'Hallo', param2: 'Andreas')
+
