be98cb47227f049a13412300792514be3de4ed6a
cccaternberg <acaternberg@cloudbees.com>
2021-02-16 19:18:18 +0100

    change repo

--- jobs/Jenkinsfile-sharedlib-maven.groovy
+++ jobs/Jenkinsfile-sharedlib-maven.groovy
@@ -1,2 +1,2 @@
 library '_github.com_pipeline-demo-caternberg_pipeline_examples' _
-mavenProject (gitRepo:"git@github.com:pipeline-demo-caternberg/maven-project.git",credentialId:"githubuserssh",branch:"master")
+mavenProject (gitRepo:"git@github.com:pipeline-demo-caternberg/maven-executable-jar-example.git",credentialId:"githubuserssh",branch:"master")
