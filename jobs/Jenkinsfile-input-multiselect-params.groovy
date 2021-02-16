0864b888f4c3cd353b33d8352ca51524ae275af8
cccaternberg <acaternberg@cloudbees.com>
2021-02-16 12:32:45 +0100

    fix agent

--- jobs/Jenkinsfile-input-multiselect-params.groovy
+++ jobs/Jenkinsfile-input-multiselect-params.groovy
@@ -43,13 +43,11 @@ pipeline {
     agent none
     stages {
         stage('Build') {
-            agent any
             steps {
                 echo "prep"
             }
         }
         stage('Select_Images') {
-            agent none
             steps {
                 script {
                     IMAGES = input message: 'Deploy?', parameters: [extendedChoice(description: '', multiSelectDelimiter: ',', name: 'images', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_MULTI_SELECT', value: 'image1,image2,image3', visibleItemCount: 10)]
