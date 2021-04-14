12f94ca4268e9afc02ce770f4742b6f1e404bbb8
cccaternberg <acaternberg@cloudbees.com>
2021-04-14 07:41:53 +0200

    add anso colour

--- jobs/Jenkinsfile-parallel.groovy
+++ jobs/Jenkinsfile-parallel.groovy
@@ -7,15 +7,20 @@ pipeline {
     stages {
         stage('init-stage') {
             steps {
-                echo "This is the init stage"
+                ansiColor('css') {
+                    echo "This is the init stage"
+                }
             }
         }
-        stage('run-parallel-branches') {
+        stage('run-parallel') {
             steps {
                 parallel(
                         a: {
                             container("curl") {
-                                echo "This is branch a"
+                                ansiColor('vga') {
+                                    echo '\033[42m\033[97mThread A green background\033[0m'
+                                }
+                                //echo "This is branch a"
                             }
                         },
                         b: {
