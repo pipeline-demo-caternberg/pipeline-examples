3299965f0b2bd85408a71438de377ca6f02c6e81
cccaternberg <acaternberg@cloudbees.com>
2022-06-10 07:13:01 +0200

    add

---
+++ vars/createAgentSimple.groovy
@@ -0,0 +1,11 @@
+def call(folderName,agentName,agentFileSystemPath,agentDescription,agentLabels,numOfExecutors)
+{
+    echo "Hello from SharedLibrary: agentName ${folderName}"
+    echo "Hello from SharedLibrary: agentName ${agentName}"
+    echo "Hello from SharedLibrary: agentFileSystemPath ${agentFileSystemPath}"
+    echo "Hello from SharedLibrary: agentDescription ${agentDescription}"
+    echo "Hello from SharedLibrary: agentLabels ${agentLabels}"
+    echo "Hello from SharedLibrary: numOfExecutors ${numOfExecutors}"
+
+    echo "Agent has been created successfully."
+}
