172b7a631e7167dd422a7a00599660ae6a04fbb5
cccaternberg <acaternberg@cloudbees.com>
2022-06-16 12:00:58 +0200

    add

---
+++ jobs/Jenkinsfile-emailext.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent {
+        kubernetes {
+            label ‘maven-app’
+            idleMinutes 5
+            yamlFile ‘podTemplates.yaml’
+            defaultContainer ‘maven’
+        }
+    }
+    stages {
+        stage(‘Checkout’) {
+            steps {
+                git credentialsId: “${github_creds}” , url: “${github_repo}”
+            }
+        }
+        stage(‘Build’) {
+            steps {
+                sh ‘mvn -B -DskipTests clean package’
+            }
+        }
+    }
+    post {
+        // Email Ext plugin:
+        success {
+            emailext (
+                    subject: “SUCCESSFUL: Job ‘${env.JOB_NAME} [${env.BUILD_NUMBER}]‘“,
+                    body: “”"<p>SUCCESSFUL: Job ‘${env.JOB_NAME} [${env.BUILD_NUMBER}]‘:</p>
+                    <p>Check console output at &QUOT;<a href=‘${env.BUILD_URL}‘>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>“”",
+                    to: “${emailRecipient}“,
+                    from: “buildNotifications@emailaddress.com”
+            )
+        }
+        failure {
+            emailext (
+                    subject: “FAILED: Job ‘${env.JOB_NAME} [${env.BUILD_NUMBER}]‘“,
+                    body: “”"<p>FAILED: Job ‘${env.JOB_NAME} [${env.BUILD_NUMBER}]‘:</p>
+                    <p>Check console output at &QUOT;<a href=‘${env.BUILD_URL}’>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>“”",
+                    to: “${emailRecipient}“,
+                    from: “buildNotifications@emailaddress.com”
+            )
+        }
+    }
+}
