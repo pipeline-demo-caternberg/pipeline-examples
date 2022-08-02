b81b8349a5c0bb2081096cbd6b502cf4c4bbd064
cccaternberg <acaternberg@cloudbees.com>
2022-08-02 14:47:35 +0200

    add dummy step

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,4 +3,5 @@ def call(name) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
     echo "Hello ${name}"
     echo "Have a great day!"
+    sh "echo $HOST"
 }
