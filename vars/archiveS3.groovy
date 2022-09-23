7244c3d6b733d4ff5382ef00e15e1ec7fea7c2c4
cccaternberg <acaternberg@cloudbees.com>
2022-09-23 14:40:58 +0200

    a

--- vars/archiveS3.groovy
+++ vars/archiveS3.groovy
@@ -4,5 +4,9 @@ def call() {
         echo "$eventCause"
         def url = eventCause[0].event.url
         echo "URL: $url"
+        def log = sh(script: """
+             curl -v  -u  ${jenkinsUserAndToken}  --silent  $url/consoletext'
+          """, returnStdout: true)
+        echo LOG: "$log"
     }
 }
