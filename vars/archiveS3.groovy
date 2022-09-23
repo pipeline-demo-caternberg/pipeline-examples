70c54ea6d981f67150751fa00bbf1b6ac97b9f7a
cccaternberg <acaternberg@cloudbees.com>
2022-09-23 13:48:56 +0200

    add

---
+++ vars/archiveS3.groovy
@@ -0,0 +1,8 @@
+def call() {
+    script {
+        def eventCause = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
+        echo "$eventCause"
+        def url = eventCause[0].event.url
+        echo "URL: $url"
+    }
+}
