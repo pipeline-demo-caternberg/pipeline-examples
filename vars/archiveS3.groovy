021bcefb784a11fa972bf98ca9209fab60027963
cccaternberg <acaternberg@cloudbees.com>
2022-09-23 17:55:12 +0200

    a

--- vars/archiveS3.groovy
+++ vars/archiveS3.groovy
@@ -3,11 +3,10 @@ def call() {
         echo "$eventCause"
         def url = eventCause[0].event.url
         echo "URL: $url"
-        echo "TEST TESTE"
        // def scriptS3 = libraryResource 'scripts/curlBuildLog.sh'
         def curlPod = libraryResource 'podtemplates/podTemplate-curl.yaml'
         def label = "curl"
-        podTemplate(name: 'curl', label: label, yaml: curlPod) {
+        podTemplate(name: 'mycurl', label: label, yaml: curlPod) {
                 node(label) {
                         sh(script: """
                                     curl -v  -u  admin:admin $url/consoleText
