1857cb26c0624bfc933f04ab571e564e6e7e1720
cccaternberg <acaternberg@cloudbees.com>
2022-09-29 14:00:08 +0200

    a

--- vars/archiveS3.groovy
+++ vars/archiveS3.groovy
@@ -4,8 +4,28 @@ def call() {
         def url = eventCause[0].event.url
         echo "URL: $url"
        // def scriptS3 = libraryResource 'scripts/curlBuildLog.sh'
+        //retrive the logs and files to be archieved
         sh(script: """
-                  curl -v  -u  admin:admin $url/consoleText
+                  ARCHIVEFOLDER=TARFOLDER
+                  mkdir -p $ARCHIVEFOLDER
+                  cd $ARCHIVEFOLDER
+                  curl -v  -u  admin:admin $url/consoleText -0
+                  ls -ltr
+                  #TODO: copy all wanted fies to here 
          """)
+        //verify
+        //TODO: add your verification impl
+
+        //packacge
+        sh(script: """
+                 cd $WORKSPACE
+                 //TODO get BUILD_TAG, GIT_COMMIT  from json event data payload
+                tar -cvzf archive-$(date +%s%3N).tar.gz TARFOLDER/*
+               
+         """)
+
+        //upload to s6
+        //
+        sh "echo up0load to S3 using aws cli "
 
 }
