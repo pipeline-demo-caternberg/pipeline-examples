1f02355d3e5566a7501b9a4cc0a5653cafa9538e
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2023-08-29 08:47:00 +0200

    Update Jenkinsfile-stash-unstash.groovy

--- jobs/Jenkinsfile-stash-unstash.groovy
+++ jobs/Jenkinsfile-stash-unstash.groovy
@@ -4,7 +4,7 @@ pipeline {
         stage('Stage Stash') {
             agent {
                 kubernetes {
-                    yamlFile 'resources/yaml/podTemplate-tools-os.yml'
+                    yamlFile 'resources/podTemplates/podTemplate-os-tools.yml'
                 }
             }
             steps {
@@ -19,7 +19,7 @@ pipeline {
         stage('Stage Unstash') {
             agent {
                 kubernetes {
-                    yamlFile 'resources/yaml/podTemplate-maven.yml'
+                    yamlFile 'resources/podTemplates/podTemplate-maven.yml'
                 }
             }
             steps {
