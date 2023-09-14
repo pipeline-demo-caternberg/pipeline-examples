8a781356dca101cede4db53ab6e39053a004dd7d
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2023-09-14 08:17:52 +0200

    Create Jenkinsfile-declarative-kubernetes.groovy
    add

---
+++ jobs/Jenkinsfile-declarative-kubernetes.groovy
@@ -0,0 +1,31 @@
+// Uses Declarative syntax to run commands inside a container.
+pipeline {
+    agent {
+        kubernetes {
+            yaml '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: shell
+    image: ubuntu
+    command:
+    - sleep
+    args:
+    - infinity
+'''
+            // Can also wrap individual steps:
+            // container('shell') {
+            //     sh 'hostname'
+            // }
+            defaultContainer 'shell'
+        }
+    }
+    stages {
+        stage('Main') {
+            steps {
+                sh 'hostname'
+            }
+        }
+    }
+}
