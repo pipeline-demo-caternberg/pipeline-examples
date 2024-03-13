163caf18e90e62f262fe7f1296b0fce8e74aad58
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2024-03-13 18:57:16 +0100

    Update Jenkins-parallel-dynamic-declaractive.groovy

--- jobs/Jenkins-parallel-dynamic-declaractive.groovy
+++ jobs/Jenkins-parallel-dynamic-declaractive.groovy
@@ -1,1 +1,33 @@
 
+// Example function that would return an array of regions to deploy
+def getRegionsToDeploy() {
+  return ["One","Two","Three"]
+}
+
+// Convert array of regions to map of stages
+def getDeployStages() {
+  return getRegionsToDeploy().collectEntries { region ->
+    [
+      (region): {
+        stage("Deploy region ${region}") {
+          echo  "sample command ${region}"
+        }
+      }
+    ]
+  }
+}
+
+pipeline {
+  agent any
+  stages {
+    // other stages...
+    stage('Deploy') {
+      steps {
+        script {
+          // Directly pass the map of stages to `parallel`
+          parallel getDeployStages()
+        }
+      }
+    }
+  }
+}
