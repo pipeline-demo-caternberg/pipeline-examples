076f4af702824e785bf6ee8d8d34a0cd38137e04
cccaternberg <acaternberg@cloudbees.com>
2024-03-22 08:54:29 +0100

    add

--- jobs/Jenkinsfile-ExtendedChoiceParamsGitBranches.groovy
+++ jobs/Jenkinsfile-ExtendedChoiceParamsGitBranches.groovy
@@ -1,1 +1,55 @@
-Jenkinsfile-ExtendedChoiceParams.groovy
+pipeline {
+    agent any
+
+    parameters {
+        extendedChoice(
+                name: 'GIT_BRANCH',
+                description: 'Select Git branch',
+                type: 'PT_SINGLE_SELECT',
+                groovyScript: [
+                        classpath: [],
+                        sandbox: false,
+                        script: '''
+                    // Import necessary classes
+                    import hudson.model.*
+                    import jenkins.model.*
+                    import groovy.json.JsonSlurper
+
+                    // Fetch Git branches dynamically
+                    def gitBranches = []
+
+                    // Get Jenkins instance
+                    def jenkins = Jenkins.instance
+
+                    // Get the project (assuming the pipeline is configured at the project level)
+                    def project = jenkins.getItemByFullName(env.JOB_NAME)
+
+                    // Get the Git SCM information from the project
+                    def scm = project.scm
+                    
+                    // Check if the SCM is Git
+                    if (scm instanceof hudson.plugins.git.GitSCM) {
+                        // Iterate over the branches defined in the Git SCM configuration
+                        scm.branches.each { branchSpec ->
+                            // Extract branch name
+                            def branchName = branchSpec.name
+                            // Add branch name to the list
+                            gitBranches.add(branchName)
+                        }
+                    }
+
+                    return gitBranches
+                '''
+                ]
+        )
+    }
+
+    stages {
+        stage('Example Stage') {
+            steps {
+                // Example stage steps
+                echo "Selected Git branch: ${params.GIT_BRANCH}"
+            }
+        }
+    }
+}
